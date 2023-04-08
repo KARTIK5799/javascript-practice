9792962f1052b21cad98ad64fa80e675d7a6ec08
KARTIK5799 <thekartiksapkal@gmail.com>
2023-04-09 03:22:47 +0530

    Q 2

--- practice set/index.js
+++ practice set/index.js
@@ -1,9 +1,16 @@
 //Write a JavaScript function that reverse a number.
-function reverse(){
+/*function reverse(){
     let a=prompt("enter the number");
     let b=a.toString().split("").reverse().join("");
 if(b.endsWith("-")){
     b="-"+parseInt(b);
 }
     alert("the reverse no is "+b);
+}
+*/
+//Write a JavaScript function that returns a passed string with letters in alphabetical order
+function alpha(){
+    let a=prompt("Enter the random alphabets");
+let b=a.split("").sort().join("");
+alert(b);
 }
