5e61b12e0ed60abcdb1cbf4115c90482bb290f96
KARTIK5799 <thekartiksapkal@gmail.com>
2023-04-09 03:04:05 +0530

    negative no

--- practice set/index.js
+++ practice set/index.js
@@ -1,6 +1,11 @@
+//Write a JavaScript function that reverse a number.
 function reverse(){
     let a=prompt("enter the number");
     let b=a.toString().split("").reverse().join("");
+if(b.endsWith("-")){
+    b="-"+parseInt(b);
 
+
+}
     alert(b);
 }
