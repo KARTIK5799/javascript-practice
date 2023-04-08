09347fbfdf5483eaef0b049a5c224bb9ea07a266
KARTIK5799 <thekartiksapkal@gmail.com>
2023-04-08 23:23:48 +0530

    p2

---
+++ 02.Primitives-and-Objects.js
@@ -0,0 +1,17 @@
+//nn bb ss u
+//n=null
+//n=number
+//b=BigInt
+//b=Boolian
+//s=string
+//s=symbol
+//u=undefined
+
+let a = null;//n=null
+let b =123;//n=number
+let d = BigInt("234");//b=BigInt
+let c =true;//b=Boolian
+let e ="harry";//s=string
+let f =Symbol("my name is kartik");//s=symbol
+let g;//u=undefined
+console.log(typeof a+" " + typeof b+" " + typeof c+" " + typeof d+" " + typeof e+" "+ typeof f+" " + typeof g)
