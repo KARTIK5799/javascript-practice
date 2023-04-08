a29dfa033c90cbc30d3fbfb0cbff1180f5f06e5a
KARTIK5799 <thekartiksapkal@gmail.com>
2023-04-09 00:43:46 +0530

    arithmetic with assignment

--- Operators-and-Expressions.js
+++ Operators-and-Expressions.js
@@ -6,4 +6,12 @@ console.log("Subtraction is "+ (b - a));//Subtraction
 console.log("Multiplication is "+a*b);//Multiplication
 console.log("Division is "+a/b);//Division
 console.log("Modulus is "+a%b);//Modulus
-console.log("Exponentiation is "+a**b);//Exponentiation
+console.log("Exponentiation is "+(a**b));//Exponentiation
+
+
+//JavaScript also has some shorthand operators that combine arithmetic with assignment. Here are some examples:
+console.log("a += b is the same as a = a + b "+(a+=b));//Addition assignment
+console.log("a -= b is the same as a = a - b "+(a-=b));//Subtraction assignment
+console.log("a *= b is the same as a = a * b "+(a*=b));//Multiplication assignment	
+console.log("a /= b is the same as a = a / b "+(a/=b));//Division assignment
+console.log("a %= b is the same as a = a % b "+(a%=b));//Modulus assignment	
