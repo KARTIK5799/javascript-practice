159a0257ee2cc4d077a59436a5dacb9663f5ef5e
KARTIK5799 <thekartiksapkal@gmail.com>
2023-04-09 00:36:14 +0530

    Arithmetic operators

---
+++ Operators-and-Expressions.js
@@ -0,0 +1,9 @@
+let a=2;
+let b=3;
+//Arithmetic operators are used to perform mathematical calculations. Here are the most common arithmetic operators in 
+console.log("Addition is "+a+b);//Addition
+console.log("Subtraction is "+ (b - a));//Subtraction
+console.log("Multiplication is "+a*b);//Multiplication
+console.log("Division is "+a/b);//Division
+console.log("Modulus is "+a%b);//Modulus
+console.log("Exponentiation is "+a**b);//Exponentiation
