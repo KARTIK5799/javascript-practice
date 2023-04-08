754df87b7c3018368bff66f72ab18667498f8d93
KARTIK5799 <thekartiksapkal@gmail.com>
2023-04-08 23:43:16 +0530

    Q 3

--- JavaScript-PracticeSet-1.js
+++ JavaScript-PracticeSet-1.js
@@ -5,3 +5,12 @@ console.log(a+b);
 
 //Use the typeof keyword to see the data type of the variables in the previous question.
 console.log(typeof a);
+
+//Create a constant object in JavaScript and then try to change its value to another data type.
+const a1={
+    name :"kartik",
+    id:2,
+    attendence:true
+}
+//a1=43; it is not possible to change the value of constant object but we can change key values.
+
