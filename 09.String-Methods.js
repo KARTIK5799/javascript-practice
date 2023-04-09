520587013b803c43e564820ad787b4f112fe27b8
KARTIK5799 <thekartiksapkal@gmail.com>
2023-04-09 17:11:45 +0530

    string  methods

---
+++ 09.String-Methods.js
@@ -0,0 +1,9 @@
+let a='kartik sapkal';
+console.log(a);
+console.log(a.length);
+console.log(a.slice(2,7));
+console.log(a.substring(5,9));
+console.log(a.replace("kartik","raj"));
+console.log(a.replaceAll("a","r"));
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
