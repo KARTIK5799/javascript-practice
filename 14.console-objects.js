11757b0602d7062d3c2576d8f5f68c823c8a4359
KARTIK5799 <thekartiksapkal@gmail.com>
2023-04-10 00:44:32 +0530

    console-obj

---
+++ 14.console-objects.js
@@ -0,0 +1,12 @@
+const obj={
+    name1 :"kartik",
+    name2:"raj",
+    name3:"yash"
+
+}
+console.clear();
+console.log(console);
+console.error("this is an error");
+console.warn("this shows waring");
+console.assert("it gives right assertions");
+console.table(obj);
