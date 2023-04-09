3f17c5afa4742e6e6f06703b52cb3d24554d59e3
KARTIK5799 <thekartiksapkal@gmail.com>
2023-04-09 20:57:12 +0530

    for each

---
+++ 12.for_loop.js
@@ -0,0 +1,14 @@
+const a= [1,2,3,4,5,6]
+
+//for-loop
+for (let i=0;i<=(a.length-1);i++){
+    a[i]=(a[i]*2); 
+}
+console.log(a);
+
+//for-each
+a.forEach((x)=>{
+    (x*=2);
+   
+});
+console.log(a);
