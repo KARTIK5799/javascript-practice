bd926d9ff3b05026415853ae990179b24a50efb1
KARTIK5799 <thekartiksapkal@gmail.com>
2023-04-09 17:45:31 +0530

    01

---
+++ 11.ArrayMethods.js
@@ -0,0 +1,8 @@
+const num=[1,2,3,4,5,6,7,8,9]
+console.log(typeof (num.toString()));
+num.pop();
+console.log(num);
+num.push(32);
+console.log(num);
+console.log(num.shift(),num);
+console.log(num.unshift(78),num);
