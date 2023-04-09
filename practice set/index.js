50d88be5402866b8d30bb055fb4595fc7039f657
KARTIK5799 <thekartiksapkal@gmail.com>
2023-04-10 00:15:52 +0530

    guess game with html

---
+++ practice set/index.js
@@ -0,0 +1,48 @@
+//Write a JavaScript function that reverse a number.
+function reverse(){
+    let a=prompt("enter the number");
+    let b=a.toString().split("").reverse().join("");
+if(b.endsWith("-")){
+    b="-"+parseInt(b);
+}
+    alert("the reverse no is "+b);
+}
+
+//Write a JavaScript function that returns a passed string with letters in alphabetical order
+function alpha(){
+    let a=prompt("Enter the random alphabets");
+let b=a.split("").sort().join("");
+alert(b);
+}
+
+
+//write a program to create random number guess game
+function random () {
+     let x=Math.floor((Math.random()*100)+1);
+     console.log(x);
+     let chance=0;
+     let y=parseInt(prompt("guess the number"));
+     chance++;
+     compare(y);
+     function compare(y){
+                if(x===y){
+                        alert(`your guess is right you guess in ${chance}`);
+                }else if(x<y){
+                        alert("your guess is greter ");
+                        y=parseInt(prompt("guess again the number"));
+                        chance++;
+                        compare(y)
+                }else if(x>y){
+                        alert("your guess is smaller ");
+                        y=parseInt(prompt("guess again the number"));
+                        chance++;
+                        compare(y)
+                }else{
+                        alert("Invalid number")
+                        y = parseInt(prompt("Enter a number"));
+                        chances++;
+                        compare(n);
+                    }
+             }
+}  
+
