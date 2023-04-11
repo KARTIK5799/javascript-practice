be72ae695ceb9ba0239bb1d19b2ea01280503bd1
KARTIK5799 <thekartiksapkal@gmail.com>
2023-04-11 15:44:51 +0530

    gg

--- snake water gun/index.js
+++ snake water gun/index.js
@@ -1,1 +1,30 @@
-alert("hello");
+let user=prompt("enter S , W, G ");
+let randumNumber=Math.floor(Math.random()*3);
+const arr=["S","W","G"][randumNumber]
+
+
+const match = (arr, user)=>{
+    if(arr === user){
+      return "Nobody"
+    }
+    else if(arr === "S" && user==="W"){
+      return "arr"
+    }
+    else if(arr === "S" && user==="G"){
+      return "user"
+    }
+    else if(arr === "G" && user==="W"){
+      return "user"
+    }
+    else if(arr === "G" && user==="S"){
+      return "arr"
+    }
+    else if(arr === "W" && user==="S"){
+      return "user"
+    }
+    else if(arr === "W" && user==="G"){
+      return "arr"
+    }
+  }
+  let result = match(arr, user)
+ console.log(`CPU:${arr} <br> User:${user} <br>The winner is: ${result.toUpperCase()}` )
