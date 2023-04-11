70faddb498b941a3a4a67c0efd4ec3f3c49c5d32
KARTIK5799 <thekartiksapkal@gmail.com>
2023-04-11 15:02:08 +0530

    change

--- practice set 2/index.js
+++ practice set 2/index.js
@@ -1,1 +1,5 @@
 document.getElementsByTagName("ul")[0].firstElementChild.style.color="red";
+
+Array.from(document.getElementsByTagName("li")).forEach((element) =>{
+    element.style.background = "cyan";
+  })  
