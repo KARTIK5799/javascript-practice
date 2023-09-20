50498a2a19b2cf34ee8a61da014931740e0ec27d
KARTIK5799 <128450641+KARTIK5799@users.noreply.github.com>
2023-09-20 11:34:07 +0000

    change done

--- js practice chai aur code/basic-02/objects.js
+++ js practice chai aur code/basic-02/objects.js
@@ -1,7 +1,37 @@
-const obj={
-    name:"kartik",
-    edu:"be",
-    stream:"computer science"
+const mySymbol = Symbol("key1");
+const obj = {
+    name: "kartik",
+    edu: "be",
+    "stream info": "computer science",
+    [mySymbol]: "jjste2"
+};
 
+// console.log(`My Name is ${obj.name} and My Education is ${obj.edu} in ${obj["stream info"]}. Just use of symbol ${obj[mySymbol]}`);
+console.log(typeof mySymbol);
+console.log(obj);
+
+// singleton
+// Object.create
+
+// object literals
+
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+obj.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+obj.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+obj.greeting = function(){
+    console.log("Hello JS user");
 }
-console.log(`My Mame is ${obj.name} and My Education is ${obj.edu} in ${obj.stream}.`);
+obj.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(obj.greeting());
+console.log(obj.greetingTwo());
