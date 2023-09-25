b7962575bfeea54ecf8d936cb98af25a5ceca3e0
KARTIK5799 <128450641+KARTIK5799@users.noreply.github.com>
2023-09-25 08:51:07 +0000

    do while loop

---
+++ js practice chai aur code/itration/two.js
@@ -0,0 +1,13 @@
+let index=0;
+while(index<10){
+    console.log(`the value is ${index}`);
+    index+=2;
+} 
+
+do{
+    console.log(`index is ${index}`)
+    index++;
+}while(index<=10){
+    console.log(`index are in while loop ${index}`)
+}
+    
