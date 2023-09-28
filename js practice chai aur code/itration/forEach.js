653406a3385df8429e480990d2dd965184111a05
KARTIK5799 <128450641+KARTIK5799@users.noreply.github.com>
2023-09-28 05:26:37 +0000

    forEach has three value in it item.index of item and complete array

---
+++ js practice chai aur code/itration/forEach.js
@@ -0,0 +1,6 @@
+const coding=["js","cpp","java","python"]
+coding.forEach((item,index,array)=>{
+    console.log(`in Given Array  ${item} is on ${index} index`)
+    console.log(`we can print complete array too like this, [${array}]`)
+
+});
