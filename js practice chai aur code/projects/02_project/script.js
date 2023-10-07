d83be360483c24fed67762165e8a91b23c7a94fa
KARTIK5799 <128450641+KARTIK5799@users.noreply.github.com>
2023-10-07 06:16:48 +0000

    bmi calculator

---
+++ js practice chai aur code/projects/02_project/script.js
@@ -0,0 +1,26 @@
+const form= document.querySelector('form');
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const height=parseInt(document.querySelector('#height').value);
+    const weight=parseInt(document.querySelector('#weight').value);
+    const result=document.querySelector('#result')
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = `Please give a valid height ${height}`;
+      } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `Please give a valid weight ${weight}`;
+      } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        
+        if(bmi<18.6){
+            results.innerHTML = `<span>You are Under Weight</span><span> ${bmi}</span>`;
+        }
+
+        if(bmi<18.6||bmi>24.9){
+            results.innerHTML = `<span>Your body is in Normal range</span><span> ${bmi}</span>`;
+        }
+        else{
+            results.innerHTML = `<span>Your body Overweight </span><span> ${bmi}</span>`;
+        }
+      }
+})
